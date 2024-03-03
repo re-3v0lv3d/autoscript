c696c08d135e310c616e137b886d9dbc837d5925
z3r0 <66015195+akjiacontos@users.noreply.github.com>
2024-03-03 16:00:53 +0100

    Update AutoScriptCunaGUI.sh

--- AutoScriptCunaGUI.sh
+++ AutoScriptCunaGUI.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-# Ventana de bienvenida
 zenity --info --text="Bienvenido al asistente de instalación automatizada" --title="Instalador"
 
-# Verificación de conexión a Internet
 if ! wget -q --spider https://google.com; then
     zenity --error --text="No hay conexión a Internet. El programa se cerrará." --title="Error"
     exit 0
 fi
 
-# Ventana para ingresar la contraseña de sudo
 password=$(zenity --password --title="Contraseña de sudo")
 
-# Actualización e instalación
 echo "$password" | sudo -S apt-get update
 echo "$password" | sudo -S apt-get install xfreerdp
 
-# Ventana de instalación completada
 zenity --info --text="Instalación terminada sin errores" --title="Éxito"
 
-# Creación del script
 usuario=$(zenity --entry --text="Introduce el nombre de usuario (Ej: TPV2):" --title="Nombre de usuario")
 touch "/home/$usuario/Escritorio/ERP.sh"
 chmod 777 "/home/$usuario/Escritorio/ERP.sh"
@@ -37,7 +31,6 @@ echo Conectando a la sesión remota
 
 xfreerdp /u:$usuario /p:123456 /v:129.100.21.1" > "/home/$usuario/Escritorio/ERP.sh"
 
-# Creación del archivo de autoarranque
 touch "/home/$usuario/.config/autostart/ERP.desktop"
 echo "[Desktop Entry]
 Type=Application
@@ -47,6 +40,5 @@ Icon=system-run
 X-GNOME-Autostart-enabled=true" > "/home/$usuario/.config/autostart/ERP.desktop"
 chmod 777 "/home/$usuario/.config/autostart/ERP.desktop"
 
-# Ventana de finalización
 zenity --info --text="Todo listo. ¡Disfruta!" --title="Éxito"
 
